6e3665586363d8039462a9dd59392a531a65b5b3
Vishal Shah <vishah2001@gmail.com>
2019-01-30 16:41:06 -0500

    template commit

---
+++ model-development/1_1_manipulate_data.py
@@ -0,0 +1,3 @@
+# This is where we will manipulate the data
+# We will put the result of this into the model
+# This will go in a pickle (.pkl) file that goes in the data directory
---
+++ model-development/2_0_train_model.py
@@ -0,0 +1,3 @@
+# This is where we will train our model
+# The model will be saved in a .pkl file in the model directory
+# We will put the model in the production pipeline after its done
